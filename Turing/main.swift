bb1c1149c5bdaf5da711b09dd6b1bf14d4ace533
DongXu <cikelengfeng@gmail.com>
2018-09-13 19:33:28 +0800

    [DEL] dev code

--- Turing/main.swift
+++ Turing/main.swift
@@ -9,16 +9,16 @@
 import Foundation
 
 func main() {
-//    guard CommandLine.argc >= 3 else {
-//        fatalError("缺少参数 processonFilePath, outputPath, classPrefix")
-//    }
+    guard CommandLine.argc >= 3 else {
+        fatalError("缺少参数 processonFilePath, outputPath, classPrefix")
+    }
     // arguments[0] is always the program_name
-    let processonFilePath = "/Users/xudong/Downloads/Light.pos"
-    let outputPath = "/Users/xudong/git/Turing/GenCodeTests/"
-    let classPrefix = "Test"
-//    let processonFilePath = CommandLine.arguments[1]
-//    let outputPath = CommandLine.arguments[2]
-//    let classPrefix = CommandLine.arguments[3]
+//    let processonFilePath = "/Users/xudong/Downloads/Light.pos"
+//    let outputPath = "/Users/xudong/git/Turing/GenCodeTests/"
+//    let classPrefix = "Test"
+    let processonFilePath = CommandLine.arguments[1]
+    let outputPath = CommandLine.arguments[2]
+    let classPrefix = CommandLine.arguments[3]
     
     let processon = ProcessonDiagram(filePath: processonFilePath, codePath: outputPath, name: classPrefix)
     let codeGen = ObjectiveCGen(source: processon)
